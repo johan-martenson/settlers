ca3392e6bc91b49b72124d5deb2af55b5203f7b2
Johan Mårtenson <johan.martenson@gmail.com>
2014-05-17 15:29:30 +0200

    Added helpers to construct houses

--- src/test/java/org/appland/settlers/test/Utils.java
+++ src/test/java/org/appland/settlers/test/Utils.java
@@ -11,6 +11,19 @@ import org.appland.settlers.model.Building;
 import org.appland.settlers.model.Material;
 import org.appland.settlers.model.Worker;
 
+import static org.appland.settlers.model.Building.ConstructionState.*;
+import static org.appland.settlers.model.Building.ConstructionState;
+
+import static org.appland.settlers.model.Material.*;
+
+import org.appland.settlers.model.Cargo;
+import org.appland.settlers.model.DeliveryNotPossibleException;
+import org.appland.settlers.model.InvalidMaterialException;
+import org.appland.settlers.model.InvalidStateForProduction;
+import org.appland.settlers.model.Sawmill;
+
+import static org.junit.Assert.assertTrue;
+
 public class Utils {
 	
 	private static Logger log = Logger.getLogger(Worker.class.getName());
@@ -32,10 +45,18 @@ public class Utils {
 	}
     }
 
+    public static void assertConstructionStateDuringFastForward(int time, Building b, ConstructionState state) {
+        int i = 0;
+        for (i = 0; i < time; i++) {
+            assertTrue(b.getConstructionState() == state);
+            b.stepTime();
+        }
+    }
+    
 	public static List<Building> getNeedForMaterial(Material wood, List<Building> buildings) {
 		log.log(Level.INFO, "Finding buildings requiring {0} in list {1}", new Object[] {wood, buildings});
 		
-		List<Building> result = new ArrayList<Building>();
+		List<Building> result = new ArrayList<>();
 		
 		for (Building b : buildings) {
 			if (b.needsMaterial(wood)) {
@@ -46,4 +67,35 @@ public class Utils {
 		
 		return result;
 	}
+
+    static void constructMediumHouse(Building sm) throws InvalidMaterialException, DeliveryNotPossibleException, InvalidStateForProduction {
+        Cargo woodCargo = Cargo.createCargo(PLANCK);
+        Cargo stoneCargo = Cargo.createCargo(STONE);
+        
+        /* Deliver 4 wood and 3 stone */
+        sm.deliver(woodCargo);
+        sm.deliver(woodCargo);
+        sm.deliver(woodCargo);
+        sm.deliver(woodCargo);
+        
+        sm.deliver(stoneCargo);
+        sm.deliver(stoneCargo);
+        sm.deliver(stoneCargo);
+        
+        Utils.fastForward(150, sm);
+    }
+
+    static void constructSmallHouse(Building wc) throws InvalidMaterialException, DeliveryNotPossibleException, InvalidStateForProduction {
+        Cargo woodCargo = Cargo.createCargo(PLANCK);
+        Cargo stoneCargo = Cargo.createCargo(STONE);
+        
+        /* Deliver 4 wood and 3 stone */
+        wc.deliver(woodCargo);
+        wc.deliver(woodCargo);
+        
+        wc.deliver(stoneCargo);
+        wc.deliver(stoneCargo);
+        
+        Utils.fastForward(100, wc);
+    }
 }
