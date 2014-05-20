70fccd738ea2fe98ea1060eb5243313b5a977b61
Johan Mårtenson <johan.martenson@gmail.com>
2014-05-20 22:47:12 +0200

    Add Farm class

---
+++ src/main/java/org/appland/settlers/model/Farm.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.appland.settlers.model;
+
+import static org.appland.settlers.model.Material.WHEAT;
+import static org.appland.settlers.model.Size.LARGE;
+
+/**
+ *
+ * @author johan
+ */
+@HouseSize(size=LARGE)
+@Production(output=WHEAT, productionTime=100, requiredGoods={})
+public class Farm extends Building{
+
+    public static Farm createFarm() {
+        return new Farm();
+    }
+    
+}
