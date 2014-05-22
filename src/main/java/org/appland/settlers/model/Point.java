3fcc22bec737ebb72e56a0bf7398a57a9f4f9c69
Johan Mårtenson <johan.martenson@gmail.com>
2014-05-22 22:29:59 +0200

    Add point class with nice toString method

---
+++ src/main/java/org/appland/settlers/model/Point.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.appland.settlers.model;
+
+/**
+ *
+ * @author johan
+ */
+public class Point extends java.awt.Point {
+
+    public Point(int x, int y) {
+        super(x, y);
+    }
+    
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+}
