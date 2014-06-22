d2ac91cfb598a75bda116c4e1a627bf3ba64ce68
Johan Mårtenson <johan.martenson@gmail.com>
2014-06-22 22:11:58 +0200

    Added missing files Barracks.java and MilitaryBuilding.java

---
+++ src/main/java/org/appland/settlers/model/Barracks.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.appland.settlers.model;
+
+import static org.appland.settlers.model.Size.SMALL;
+
+@HouseSize(size = SMALL)
+@MilitaryBuilding(maxHostedMilitary = 2)
+public class Barracks extends Building {
+}
---
+++ src/main/java/org/appland/settlers/model/MilitaryBuilding.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.appland.settlers.model;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ *
+ * @author johan
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MilitaryBuilding {
+
+    int maxHostedMilitary();
+}
