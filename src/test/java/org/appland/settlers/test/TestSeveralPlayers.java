b73a5ba0396692a167bc52b63205f39a5ecc6b46
Johan Mårtenson <johan.martenson@gmail.com>
2014-11-18 21:00:51 +0100

    Add test to verify that it's possible to choose a single player

--- src/test/java/org/appland/settlers/test/TestSeveralPlayers.java
+++ src/test/java/org/appland/settlers/test/TestSeveralPlayers.java
@@ -10,6 +10,7 @@ import java.util.List;
 import org.appland.settlers.model.GameMap;
 import org.appland.settlers.model.Player;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 /**
  *
@@ -25,5 +26,22 @@ public class TestSeveralPlayers {
 
         /* Create game map choosing no players */
         GameMap map = new GameMap(players, 20, 20);
+    }
+
+    @Test
+    public void testChooseOnePlayer() throws Exception {
+
+        /* Create player list with one player */
+        List<Player> players = new LinkedList<>();
+        players.add(new Player("Player 0"));
+
+        /* Create game map choosing no players */
+        GameMap map = new GameMap(players, 20, 20);
+
+        /* Verify that there is one player in the game map */
+        assertEquals(map.getPlayers().size(), 1);
+
+        /* Verify that the one player is correct */
+        assertEquals(map.getPlayers().get(0), players.get(0));
     }
 }
