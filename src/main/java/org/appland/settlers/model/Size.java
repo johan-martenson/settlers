9958ee65dba9a7df3334e3ee3718100f71c97b9d
Johan Mårtenson <johan.martenson@gmail.com>
2015-01-04 19:44:39 +0100

    Added a utility function to determine if a given available spot can contain a new building

--- src/main/java/org/appland/settlers/model/Size.java
+++ src/main/java/org/appland/settlers/model/Size.java
@@ -1,6 +1,30 @@
 package org.appland.settlers.model;
 
-public enum Size {
+public enum Size implements Comparable<Size> {
 
-    SMALL, MEDIUM, LARGE
+    SMALL, MEDIUM, LARGE;
+
+    /**
+     * Compares two Size instances and returns true if the second instance
+     * fits within the first instance.
+     * 
+     * @param s1 Instance of Size
+     * @param s2 Instance of Size
+     * @return true if s2 fits in s1
+     */
+    static public boolean contains(Size s1, Size s2) {
+        if (s1 == s2) {
+            return true;
+        }
+
+        if (s1 == MEDIUM) {
+            if (s2 == LARGE) {
+                return false;
+            }
+
+            return true;
+        }
+
+        return true;
+    }
 }
