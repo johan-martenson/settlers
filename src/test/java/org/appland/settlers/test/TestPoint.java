6c4f7a33c9b4992a7cbcada74da5da6a7eb319dd
Johan Mårtenson <johan.martenson@gmail.com>
2017-11-05 16:15:05 +0100

    Added test to increase coverage for Point

--- src/test/java/org/appland/settlers/test/TestPoint.java
+++ src/test/java/org/appland/settlers/test/TestPoint.java
@@ -26,6 +26,7 @@ public class TestPoint {
         assertFalse(right.isLeftOf(point));
         assertFalse(upRight.isLeftOf(point));
         assertFalse(downRight.isLeftOf(point));
+        assertFalse(left.up().isLeftOf(point));
 
         /* Verify isRightOf */
         assertTrue(right.isRightOf(point));
@@ -34,6 +35,7 @@ public class TestPoint {
         assertFalse(downLeft.isRightOf(point));
         assertFalse(upRight.isRightOf(point));
         assertFalse(downRight.isRightOf(point));
+        assertFalse(right.up().isRightOf(point));
 
         /* Verify isUpLeftOf */
         assertTrue(upLeft.isUpLeftOf(point));
@@ -66,5 +68,53 @@ public class TestPoint {
         assertFalse(downLeft.isDownRightOf(point));
         assertFalse(right.isDownRightOf(point));
         assertFalse(upRight.isDownRightOf(point));
+    }
+
+    @Test
+    public void testPointIsAdjacent() {
+
+        /* Verify adjacent points*/
+        Point point = new Point(10, 10);
+        Point left = new Point(8, 10);
+        Point right = new Point(12, 10);
+        Point upLeft = new Point(9, 11);
+        Point downLeft = new Point(9, 9);
+        Point upRight = new Point(11, 11);
+        Point downRight = new Point(11, 9);
+        Point up = new Point(10, 12);
+        Point down = new Point(10, 8);
+
+        assertTrue(point.isAdjacent(left));
+        assertTrue(point.isAdjacent(right));
+        assertTrue(point.isAdjacent(upLeft));
+        assertTrue(point.isAdjacent(downLeft));
+        assertTrue(point.isAdjacent(upRight));
+        assertTrue(point.isAdjacent(downRight));
+        assertTrue(point.isAdjacent(up));
+        assertTrue(point.isAdjacent(down));
+    }
+
+    @Test
+    public void testNonAdjacentPoints() {
+
+        /* Verify non adjacent points*/
+        Point point = new Point(10, 10);
+        Point left = new Point(6, 10);
+        Point right = new Point(14, 10);
+        Point upLeft = new Point(8, 12);
+        Point downLeft = new Point(8, 8);
+        Point upRight = new Point(12, 12);
+        Point downRight = new Point(12, 8);
+        Point up = new Point(10, 14);
+        Point down = new Point(10, 6);
+
+        assertFalse(point.isAdjacent(left));
+        assertFalse(point.isAdjacent(right));
+        assertFalse(point.isAdjacent(upLeft));
+        assertFalse(point.isAdjacent(downLeft));
+        assertFalse(point.isAdjacent(upRight));
+        assertFalse(point.isAdjacent(downRight));
+        assertFalse(point.isAdjacent(up));
+        assertFalse(point.isAdjacent(down));
     }
 }
