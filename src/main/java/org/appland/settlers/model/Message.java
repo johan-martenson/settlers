2b437e25149514f72903f0c323c8d7eadf387b93
Johan Mårtenson <johan.martenson@gmail.com>
2020-05-11 16:26:24 +0200

    Remove outdated comment.

--- src/main/java/org/appland/settlers/model/Message.java
+++ src/main/java/org/appland/settlers/model/Message.java
@@ -2,8 +2,6 @@ package org.appland.settlers.model;
 
 public interface Message {
 
-    // Missing: this building has caused you to lose land, we're being bombarded by a catapult, emergency production program activated/deactivated
-
     enum MessageType {
         MILITARY_BUILDING_READY,
         MILITARY_BUILDING_OCCUPIED,
@@ -14,7 +12,9 @@ public interface Message {
         BUILDING_CAPTURED,
         STORE_HOUSE_IS_READY,
         TREE_CONSERVATION_PROGRAM_ACTIVATED,
-        MILITARY_BUILDING_CAUSED_LOST_LAND, TREE_CONSERVATION_PROGRAM_DEACTIVATED
+        MILITARY_BUILDING_CAUSED_LOST_LAND,
+        TREE_CONSERVATION_PROGRAM_DEACTIVATED,
+        BOMBARDED_BY_CATAPULT
     }
 
     MessageType getMessageType();
